d75e66b5a51a97d05e8e7864429f093286a198a8
Shreeyanshi Gupta <158245215+shreeyanshi123@users.noreply.github.com>
2026-01-09 16:36:06 +0530

    redux fix

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -15,30 +15,29 @@ const authSlice = createSlice({
     logout(state) {
       state.user = null;
       state.isAuthenticated = false;
+      state.error = null;
     },
   },
   extraReducers: (builder) => {
     builder
-      // 🔄 pending
       .addCase(registerUser.pending, (state) => {
         state.isLoading = true;
         state.error = null;
       })
-
-      // ✅ success
       .addCase(registerUser.fulfilled, (state, action) => {
         state.isLoading = false;
+        state.error = null; // ✅ FIX
         state.user = action.payload.user;
         state.isAuthenticated = true;
       })
-
-      // ❌ error
       .addCase(registerUser.rejected, (state, action) => {
         state.isLoading = false;
         state.error = action.payload;
       });
   },
 });
+
+
 
 export const { logout } = authSlice.actions;
 export default authSlice.reducer;
